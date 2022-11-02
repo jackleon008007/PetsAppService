a89ef97fcaf2b99cb688824fa82e9f2fb104d5c6
dylandSteven <stevendylandsaldana@gmail.com>
2022-11-01 20:18:27 -0500

    'changing'

--- GrupoWebBackend/DomainDistrict/Services/DistrictService.cs
+++ GrupoWebBackend/DomainDistrict/Services/DistrictService.cs
@@ -32,55 +32,6 @@ namespace GrupoWebBackend.DomainDistrict.Services
             return await _districtRepository.FindAsync(id);
         }
         
-        public async Task<SaveDistrictResponse> AddAsync(District district)
-        {
-            try
-            {
-                await _districtRepository.AddAsync(district);
-                await _unitOfWork.CompleteAsync();
-                return new SaveDistrictResponse(district);
-            }
-            catch (Exception e)
-            {
-                return new SaveDistrictResponse($"An error occurred while saving Category: {e.Message}");
-            }
-        }
-
-        public async Task<DistrictResponse> UpdateAsync(District district, int id)
-        {
-            var existingDistrict = await _districtRepository.FindAsync(id);
-            if (existingDistrict == null)
-                return new DistrictResponse("District not found");
-            existingDistrict.DistrictName = district.DistrictName;
-
-            try
-            {
-                _districtRepository.UpdateAsync(existingDistrict);
-                await _unitOfWork.CompleteAsync();
-                return new DistrictResponse(existingDistrict);
-            }
-            catch (Exception e)
-            {
-                return new DistrictResponse($"An error occurred while saving District: {e.Message}");
-            }
-        }
-
-        public async Task<DistrictResponse> DeleteAsync(int id)
-        {
-            var existingDistrict = await _districtRepository.FindAsync(id);
-            if (existingDistrict == null)
-                return new DistrictResponse("District not found.");
-            try
-            {
-                _districtRepository.Delete(existingDistrict);
-                await _unitOfWork.CompleteAsync();
-                return new DistrictResponse(existingDistrict);
-            }
-            catch (Exception e)
-            {
-                return new DistrictResponse($"An error occurred while deleting the pet: {e.Message}");
-            }
-        }
-    }
+       
 }
 
